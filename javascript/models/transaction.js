de8bc512eb3deb4155548fd8da7e970cc3c771a2
botto54 <bryanlbottorff@gmail.com>
2020-12-21 18:21:36 -0500

    add class objects to constructor method for transaction file next step it to build out render functions to place in constructor

---
+++ javascript/models/transaction.js
@@ -0,0 +1,9 @@
+class Transaction {
+    constructor(id, t_name, description, value, account_id){
+        this.id = id
+        this.t_name = t_name
+        this.description = description
+        this.value = value
+        this.account_id = account_id
+    }
+}
