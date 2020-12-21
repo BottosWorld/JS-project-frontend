fb21d1689c9d8967f44ecc0638036bba3c5aff07
botto54 <bryanlbottorff@gmail.com>
2020-12-21 18:14:20 -0500

    build out account class constructor, manipulate dom with render account method, return html on index page with account html method, add click event listeners to render account method, add createtransactions method to add a new fetch request for crud transaction functionality, add transaction class constructor but not finished yet, need to complete transaction file now

---
+++ javascript/models/account.js
@@ -0,0 +1,43 @@
+class Account {
+    constructor(id, account_name, balance, account_type){
+        this.id = id
+        this.account_name = account_name
+        this.balance = balance
+        this.account_type = account_type
+        this.renderAccount()
+        //render instance above to page
+    }
+
+    //render instance method here to create div, append to page, and add classes/crud form to edit/eventlisteners
+
+    renderAccount(){
+        const accountHolder = document.getElementById("account-list")
+        const accountContainer = document.createElement('div')
+        accountContainer.dataset.id = this.id
+        accountContainer.id = this.id
+        accountContainer.classList.add = 'account-history'
+        accountContainer.innerHTML += this.accountHTML()
+        accountHolder.appendChild(accountContainer)
+
+        accountContainer.addEventListener("click", e => {
+            if (e.target.className === "transaction-button") this.createTransactions(e)
+            if (e.target.className === "add-funds-button") this.createTransactions(e)
+        } )
+
+    }
+
+    accountHTML(){
+        return `
+        <h2 class="account-name">${this.account_name}</h2>
+        <h3 class="balance">$ ${this.balance}</h3>
+        <h4 class="account-type">${this.account_type}</h4> 
+        <button type="button" class="transaction-button" data-id=${this.id}>See the transaction history for this account!</button>
+         `
+    }
+
+    createTransactions(e){
+        let id = e.target.dataset.id
+        //add fetch request to retrieve transactions per account(account transaction history) scope out transactions charges and credits for assigned account
+
+    }
+}
